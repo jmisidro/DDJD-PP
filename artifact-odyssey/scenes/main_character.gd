c09154f104c029de03fb476dc658b2dcad09cb0e
José Miguel Isidro <93340308+jmisidro@users.noreply.github.com>
2025-02-14 11:07:41 +0000

    added character animations (idle, running jumping and falling)

--- artifact-odyssey/scenes/main_character.gd
+++ artifact-odyssey/scenes/main_character.gd
@@ -3,12 +3,22 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity and Animations
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if velocity.y < 0:  # Moving upwards
+			animated_sprite_2d.animation = "jumping"
+		else:  # Moving downwards
+			animated_sprite_2d.animation = "falling"
+	elif abs(velocity.x) > 1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -23,3 +33,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	var isLeft = velocity.x < 0 
+	animated_sprite_2d.flip_h = isLeft
