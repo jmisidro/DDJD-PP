3529b8bd0b171f4f8312c647753608952a326a91
José Miguel Isidro <93340308+jmisidro@users.noreply.github.com>
2025-02-17 15:47:03 +0000

    tweaking some values

--- artifact-odyssey/scenes/robo_dob.gd
+++ artifact-odyssey/scenes/robo_dob.gd
@@ -9,7 +9,7 @@ const PATROL_AREA = Vector2(200, 0) # Distance from the starting point to patrol
 const CHASE_SPEED = 250.0
 
 # Variables
-var direction = 1 # 1 = right, -1 = left
+var direction = -1 # 1 = right, -1 = left
 var start_position
 var is_chasing = false
 var player = null
@@ -17,29 +17,35 @@ var player = null
 # Called when the node is added to the scene
 func _ready() -> void:
 	start_position = position
+	ray_cast_2d.target_position = Vector2(-50, 0) # Facing Left
+	animated_sprite_2d.animation = "walking"
 
 # Physics update
 func _physics_process(delta: float) -> void:
 	if is_chasing and player:
-		# Chase the player
-		var to_player = player.position - position
-		velocity.x = sign(to_player.x) * CHASE_SPEED
-		
-		# Flip the sprite based on direction
-		animated_sprite_2d.flip_h = velocity.x < 0
-		
+		# Move directly towards the player's X position
+		if player.position.x < position.x:
+			velocity.x = -CHASE_SPEED  # Move Left
+			animated_sprite_2d.flip_h = false
+		else:
+			velocity.x = CHASE_SPEED  # Move Right
+			animated_sprite_2d.flip_h = true
+				
 	else:
 		# Patrol between the defined area
 		velocity.x = direction * SPEED
 		
 		if position.x > start_position.x + PATROL_AREA.x:
+			# Move to the left
 			direction = -1
 			animated_sprite_2d.flip_h = false
-			
+			ray_cast_2d.target_position = Vector2(-PATROL_AREA.x, 0)  # Pointing Left
 		elif position.x < start_position.x - PATROL_AREA.x:
+			# Move to the right	
 			direction = 1
 			animated_sprite_2d.flip_h = true
-
+			ray_cast_2d.target_position = Vector2(PATROL_AREA.x, 0)  # Pointing Right
+		
 	move_and_slide()
 
 # Detection logic
