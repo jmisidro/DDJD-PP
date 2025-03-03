41271acaa5f3e05ee9737e6bc99c50f3ae6d2606
Nuno <up201807530@fe.up.pt>
2025-03-03 22:50:30 +0000

    Artifacts

---
+++ artifact-odyssey/scenes/artifact.gd
@@ -0,0 +1,23 @@
+@tool
+extends Area2D
+
+@onready var game_manager = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var artifact_texture: Texture:
+	set(value):
+		artifact_texture = value
+		update_texture()
+
+		
+
+
+func _ready():
+	update_texture()
+
+func update_texture():
+	if $Sprite2D:
+		$Sprite2D.texture = artifact_texture
+
+func _on_body_entered(body):
+	game_manager.add_artifact()
+	animation_player.play("pickArtifact")
