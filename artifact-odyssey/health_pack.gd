ebcd32e53c4cd2ebb4fff519b768dc51411e96c0
Nuno <up201807530@fe.up.pt>
2025-03-04 16:16:40 +0000

    Sounds for health and chest

--- artifact-odyssey/health_pack.gd
+++ artifact-odyssey/health_pack.gd
@@ -1,6 +1,7 @@
 extends Area2D
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
 	body.health = body.MAX_HEALTH
 	print("Restored player max health")
-	queue_free()
+	animation_player.play("pickHealth")
