bd504ff8f933e96b61b20079c8b7cd5a8668485c
Nuno <up201807530@fe.up.pt>
2025-03-05 11:24:48 +0000

    Changed portals

--- artifact-odyssey/portal.gd
+++ artifact-odyssey/portal.gd
@@ -2,8 +2,13 @@ extends Area2D
 class_name Portal
 
 @export var target_portal: Portal
+@onready var game_manager = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	if (target_portal):
+	if (target_portal  && !game_manager.traveling_portal):
 		body.position = target_portal.position
-		
+		game_manager.traveling_portal = true
+
+
+func _on_body_exited(body):
+	game_manager.traveling_portal = false
