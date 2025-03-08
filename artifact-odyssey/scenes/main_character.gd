73c89f8f6ec1d96822dd417c6df12876840c4d2d
Nuno <up201807530@fe.up.pt>
2025-03-08 23:57:12 +0000

    Fix position

--- artifact-odyssey/scenes/main_character.gd
+++ artifact-odyssey/scenes/main_character.gd
@@ -186,7 +186,7 @@ func _physics_process(delta: float) -> void:
 			velocity.y = FLY_SPEED
 
 	# Change velocity drag
-	if (global_position.y < -1000):
+	if (global_position.y < -2100):
 		velocity_drag = 6
 	else:
 		velocity_drag = 1
