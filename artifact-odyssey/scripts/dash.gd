7c6e9b423ee43715835956fb7f3bef9baec45d32
José Miguel Isidro <93340308+jmisidro@users.noreply.github.com>
2025-03-10 17:14:29 +0000

    added powerup labels

--- artifact-odyssey/scripts/dash.gd
+++ artifact-odyssey/scripts/dash.gd
@@ -2,7 +2,15 @@ extends Area2D
 
 @onready var game_manager = %GameManager
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var ability_label: Label = $AbilityLabel
 
 func _on_body_entered(body):
 	body.grant_dash_ability()
 	animation_player.play("pickArtifact")
+
+	# Show the label
+	ability_label.visible = true
+
+	# Hide it after 5 seconds
+	await get_tree().create_timer(5.0).timeout
+	ability_label.visible = false
