417e2766444662c723e1f2e57dbf7383ba6675d3
Nuno <up201807530@fe.up.pt>
2025-03-16 18:28:07 +0000

    Trying sound for robo dog

--- artifact-odyssey/scenes/robo_dog.gd
+++ artifact-odyssey/scenes/robo_dog.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 const SPEED = 120
 var direction = -1
-var speed_multipler = 1
+var speed_multiplier = 1
+var is_sprinting = false
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
@@ -11,28 +12,48 @@ var speed_multipler = 1
 @onready var ray_cast_left_bottom = $RayCastLeftBottom
 @onready var ray_cast_player_l = $RayCastLeftPlayer
 @onready var ray_cast_player_r = $RayCastRightPlayer
+@onready var walk_sound = $WalkSound
+@onready var sprint_sound = $SprintSound
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Check if the player is detected in front
+	var player_detected = (ray_cast_player_r.is_colliding() and direction == 1) or (ray_cast_player_l.is_colliding() and direction == -1)
 
-	if (ray_cast_player_r.is_colliding() && direction == 1):
-		animated_sprite.animation = "running"
-		speed_multipler = 4
-	
-	if (ray_cast_player_l.is_colliding() && direction == -1):
-		animated_sprite.animation = "running"
-		speed_multipler = 4
-		
-	if (ray_cast_right.is_colliding() || !ray_cast_right_bottom.is_colliding()):
-		direction = -1
-		speed_multipler = 1
-		animated_sprite.animation = "walking"
-		animated_sprite.flip_h = false
-		
-	if (ray_cast_left.is_colliding() || !ray_cast_left_bottom.is_colliding()):
-		direction = 1
-		speed_multipler = 1
-		animated_sprite.animation = "walking"
-		animated_sprite.flip_h = true
+	if player_detected:
+		start_sprinting()
 
-	position.x += direction * SPEED * delta * speed_multipler
+	# Handle turning conditions
+	if ray_cast_right.is_colliding() or !ray_cast_right_bottom.is_colliding():
+		change_direction(-1)
+		stop_sprinting()
+	elif ray_cast_left.is_colliding() or !ray_cast_left_bottom.is_colliding():
+		change_direction(1)
+		stop_sprinting()
+
+	# Move enemy
+	position.x += direction * SPEED * delta * speed_multiplier
+
+# Function to handle sprinting
+func start_sprinting():
+	if not is_sprinting:
+		is_sprinting = true
+		speed_multiplier = 4
+		animated_sprite.animation = "running"
+		walk_sound.stop()
+		sprint_sound.play()
+
+# Function to stop sprinting
+func stop_sprinting():
+	if is_sprinting:
+		is_sprinting = false
+		speed_multiplier = 1
+		animated_sprite.animation = "walking"
+		walk_sound.play()
+		sprint_sound.stop()
+
+# Function to handle direction change
+func change_direction(new_direction):
+	if direction != new_direction:
+		direction = new_direction
+		animated_sprite.flip_h = (direction == 1)  # Flip sprite for correct direction
+		stop_sprinting()  # Reset sprint state when turning
